6d800e07b3e69bdba6976b1c265d190564bc1d89
M <m@gmail.com>
2021-06-13 19:00:28 +0100

    update

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="productView" >
+  <div class="productView container" >
     <div class="content is-medium">
       <h3>Charcoal Grilled {{userMeal.productName}}</h3>
-      <h4>GHS {{userMeal.productPrice}}.00</h4>
       
       <p>{{userMeal.productDescription}}</p>
 
       <p>What you get</p>
-
+      <div class="more">
       <ol type="1">
         <li>
           Grilled {{userMeal.productName}} neatly wrapped in foil and packaging paper
@@ -22,22 +21,18 @@
           Tissue to wipe you clean
         </li>
       </ol>
+      </div>
 
-      <div class="">
+
+      <h4>GHS {{userMeal.productPrice}}.00</h4>
+      <div class="controlSet">
         <button class="button" @click="decreaseQuantity">-</button>
-        <p>{{userMeal.productQuantity}}</p>
+        <p class="quantity">{{userMeal.productQuantity}}</p>
         <button class="button" @click="increaseQuantity">+</button>
       </div>
 
-      <button class="button is-primary" @click="updateCart(userMeal)">Add to Cart</button>
+      <button class="button is-primary is-medium" @click="updateCart(userMeal)">Add to Cart</button>
 
-      <!-- <div class="field flex">
-        <div class="control">
-          <input class="" type="checkbox">
-        </div>
-        
-        <label class="label">Banku + Pepper</label>
-      </div> -->
     </div>
   </div>
   
