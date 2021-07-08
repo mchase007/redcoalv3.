f57998af2161bb08e9a1797d3619012e7f54675d
M <m@gmail.com>
2021-07-08 07:50:50 +0100

    retest site

--- functions/test3.js
+++ functions/test3.js
@@ -1,52 +1,70 @@
-const https = require('https')
+exports.handler = async (event, context, callback) => {
 
-const options = {
+  // const firestore = admin.firestore();
+  // let data = JSON.parse(event.body);
+  // let collection = await firestore.collection("Orders").doc(data.reference)
+  // collection.set(data)
+  // let collection = await firestore.collection("Orders");
+  // let doc = collection.docs[0].ref;
+  // doc = await doc.get();
 
-  hostname: 'api.paystack.co',
+  // console.log(doc.data());
 
-  port: 443,
-
-  path: '/transaction/verify/:reference',
-
-  method: 'GET',
-
-  headers: {
-
-    Authorization: 'sk_test_47b9251f7c6f8988c1b0e045368bd50c3886cff8'
-
-  }
-
+  return callback(null, {
+  statusCode: 200,
+  // body: JSON.stringify(data)
+  }) 
 }
 
-exports.handler = (event, context, callback) => {
+// const https = require('https')
 
-  https.request(options, res => {
+// const options = {
 
-    let data = ''
+//   hostname: 'api.paystack.co',
+
+//   port: 443,
+
+//   path: '/transaction/verify/:reference',
+
+//   method: 'GET',
+
+//   headers: {
+
+//     Authorization: 'sk_test_47b9251f7c6f8988c1b0e045368bd50c3886cff8'
+
+//   }
+
+// }
+
+// exports.handler = (event, context, callback) => {
+
+//   https.request(options, res => {
+
+//     let data = ''
   
-    res.on('data', (chunk) => {
+//     res.on('data', (chunk) => {
   
-      data += chunk
+//       data += chunk
   
-    });
+//     });
   
-    res.on('end', () => {
-      // var body = Buffer.concat(chunks);
-      // console.log(data.toString());
-      console.log(JSON.parse(data))
+//     res.on('end', () => {
+//       // var body = Buffer.concat(chunks);
+//       // console.log(data.toString());
+//       console.log(JSON.parse(data))
   
-    })
+//     })
   
-  }).on('error', error => {
+//   }).on('error', error => {
   
-    console.error(error)
+//     console.error(error)
   
-  })
+//   })
 
   // const viewPacked = JSON.parse(event.body)
   // return callback(null, {
   // statusCode: 200,
   // body: JSON.stringify({msg: viewPacked})
   // }) 
-  }
+  // }
 
